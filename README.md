ceebfeae69a1cdc5e6121b15875509a1ff7a31a9
pradeeprajkk <pradeep.raj@accionlabs.com>
2025-01-21 12:27:26 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Simple Google Custom Map View with TypeScript and Parcel
+
+Before run, install parcel-bundler
+<br/>
+"npm install -g parcel-bundler"
+<br/>
+
+To Run,
+<br/>
+
+"npx parcel index.html"
+<br/><br/>
+
+![alt text](image.png)
---
+++ src/CustomMap.ts
@@ -0,0 +1,44 @@
+// Instructions to every other class
+// or how they can be an argument to 'addMarker'
+export interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+  markerContent(): string;
+  color: string;
+}
+
+export class CustomMap {
+  private googleMap: google.maps.Map;
+
+  constructor(divId: string) {
+    this.googleMap = new google.maps.Map(getElement(divId), {
+      zoom: 1,
+      center: { lat: 0, lng: 0 },
+    });
+  }
+
+  addMarker(mappable: Mappable): void {
+    const marker = new google.maps.Marker({ 
+      map: this.googleMap,
+      position: {
+        lat: mappable.location.lat,
+        lng: mappable.location.lng,
+      }
+    });
+
+    marker.addListener('click', () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: mappable.markerContent(),
+      });
+
+      infoWindow.open(this.googleMap, marker);
+    });
+  }
+}
+
+const getElement = (id: string): any => {
+  return document.getElementById(id);
+
+}
